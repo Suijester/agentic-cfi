57613043a08bb8ba0991d3d5ccc3cf676af68d80
Suijester <akilesh.yes@gmail.com>
2026-05-25 13:05:49 -0700

    feat: small pipeline change to evaluate immediately so i don't have to swap branches

--- pipeline.py
+++ pipeline.py
@@ -16,6 +16,11 @@ def run_pipeline(target_dir: str):
 
     git("add", "-A")
     git("commit", "--allow-empty", "-m", f"blue agent: {target_name}")
+
+    result = evaluate(target_dir)
+    if (result.get("passed") == False):
+        printf("Blue agent failed to properly analyze target directory.")
+
     git("checkout", "main")
 
 def clean_branches():
